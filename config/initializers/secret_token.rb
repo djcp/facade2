0c400d45e25b9508158a60510f7ee49d2cdce58d
djcp <dan@collispuro.net>
2012-04-17 16:30:39 -0400

    Initial framework sans Test::Unit and ActiveRecord

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Facade2::Application.config.secret_token = '719aa841dc7bc2ea0623dc82de94ac55eb2ead6cbec3de53550d71899d0497846a9c636e4760f3409d355242e82400fe26283d2664c23bd922bedf359a98fbb8'
