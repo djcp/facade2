50180f51eb708faca5214d6fe5f53f9016535265
djcp <dan@collispuro.net>
2012-04-30 16:59:46 -0400

    Query tweaking.

--- spec/sesame_server.rb
+++ spec/sesame_server.rb
@@ -48,18 +48,17 @@ module Facade2::Server
     end
 
     it 'should be able to find titles' do
-#      query = RDF::Query.new({
-#        :pim => {
-##          RDF.type => RDF::URI.new('http://libraries.mit.edu/ontologies/pim/pim1.0#File'),
-##          RDF::DC.title => :title,
-#          :pim => RDF::URI.new('usip-sample:FILE-64')
-#      }
-#      })
+      query = RDF::Query.new({
+        :project => {
+          RDF.type => RDF::URI.new('http://libraries.mit.edu/ontologies/pim/pim1.0#Project'),
+          RDF::URI.new('http://libraries.mit.edu/ontologies/pim/pim1.0#File') => :file
+      }
+      })
 
-      query = RDF::Query.new do
-        pattern [:project, RDF.type, RDF::URI.new('http://libraries.mit.edu/ontologies/pim/pim1.0#Project')]
-       # pattern [:file, RDF.type, RDF::URI.new('usip-sample:FILE-73')]
-      end
+#      query = RDF::Query.new do
+#        pattern [:project, RDF.type, RDF::URI.new('http://libraries.mit.edu/ontologies/pim/pim1.0#Project')]
+#        pattern [:title, RDF::DC.label, RDF.title]
+#      end
 
       solutions = query.execute(@repo)
 
